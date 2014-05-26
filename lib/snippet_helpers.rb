2ab8633c1b54f0148789c9b1ea1d9d6de3878d6c
Calle Erlandsson <calle@thoughtbot.com>
2014-05-26 20:54:53 +0200

    Fix broken snippet rendering

--- lib/snippet_helpers.rb
+++ lib/snippet_helpers.rb
@@ -1,47 +1,74 @@
 module SnippetHelpers
   SOURCE_DIR = File.expand_path('../../source', __FILE__)
 
-  SNIPPET_LANGUAGES = {
-    markup: {
-      path_segments: [SOURCE_DIR],
-      extension: 'html.erb',
-    },
-    scss: {
-      path_segments: [SOURCE_DIR, 'stylesheets', 'refills'],
-      extension: 'scss',
-    },
-    javascript: {
-      path_segments: [SOURCE_DIR, 'javascripts', 'refills'],
-      extension: 'js',
-    },
-  }
+  class Snippet
+    def initialize(name)
+      @name = name
+    end
+
+    private
+
+    attr_reader :name
+  end
+
+  class HtmlSnippet < Snippet
+    def path
+      snippet_filename = "_#{name.gsub(/-/, '_')}.html.erb"
+
+      File.join(SOURCE_DIR, snippet_filename)
+    end
+
+    def language_name
+      'markup'
+    end
+  end
+
+  class ScssSnippet < Snippet
+    def path
+      snippet_filename = "_#{name.gsub(/_/, '-')}.scss"
+
+      File.join(SOURCE_DIR, 'stylesheets', 'refills', snippet_filename)
+    end
+
+    def language_name
+      'scss'
+    end
+  end
+
+  class JavaScriptSnippet < Snippet
+    def path
+      snippet_filename = "#{name.gsub(/-/, '_')}.js"
+
+      File.join(SOURCE_DIR, 'javascripts', 'refills', snippet_filename)
+    end
+
+    def language_name
+      'javascript'
+    end
+  end
 
   def code_for(snippet_name)
     partial 'code', locals: { snippets: snippets_for(snippet_name) }
   end
 
-  private_constant :SOURCE_DIR, :SNIPPET_LANGUAGES
+  private_constant :SOURCE_DIR
 
   private
 
   def snippets_for(name)
-    SNIPPET_LANGUAGES.map do |language, properties|
-      snippet snippet_path(name, properties), language
+    snippets(name).map do |snippet|
+      if File.exists?(snippet.path)
+        partial 'snippet', locals: {
+          snippet: File.read(snippet.path),
+          language: snippet.language_name,
+        }
+      end
     end.join("\n")
   end
 
-  def snippet(path, language)
-    if File.exists?(path)
-      partial 'snippet', locals: {
-        snippet: File.read(path),
-        language: language,
-      }
+  def snippets(name)
+    [HtmlSnippet, ScssSnippet, JavaScriptSnippet].map do |snippet_factory|
+      snippet_factory.new(name)
     end
-  end
-
-  def snippet_path(snippet_name, language_properties)
-    snippet_filename = "_#{snippet_name}.#{language_properties[:extension]}"
-
-    File.join(*language_properties[:path_segments], snippet_filename)
   end
 end
